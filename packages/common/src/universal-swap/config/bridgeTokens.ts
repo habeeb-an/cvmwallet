3bbeeb40e4c8ccf1ff14296c7b079e0ff7d98a5a
habeeb-an <hbareacode@gmail.com>
2024-01-11 18:14:48 +0530

    edited oraichain ro cvm till swap-bridge excess code commenting out

--- packages/common/src/universal-swap/config/bridgeTokens.ts
+++ packages/common/src/universal-swap/config/bridgeTokens.ts
@@ -11,10 +11,7 @@ import {
 
 export type EvmDenom = 'bep20_orai' | 'bep20_airi' | 'erc20_orai' | 'kawaii_orai';
 
-export type UniversalSwapType =
-  | 'other-networks-to-oraichain'
-  | 'oraichain-to-oraichain'
-  | 'oraichain-to-other-networks';
+export type UniversalSwapType = 'other-networks-to-cvm' | 'cvm-to-cvm' | 'cvm-to-other-networks';
 
 export type TokenItemType = {
   name: string;
@@ -45,7 +42,7 @@ const minAmountSwapMap = {
 };
 
 export const getTokensFromNetwork = (network: CustomChainInfo): TokenItemType[] => {
-  return network.currencies.map(currency => ({
+  return network.currencies.map((currency) => ({
     name: currency.coinDenom,
     org: network.chainName,
     coinType: network.bip44.coinType,
@@ -70,38 +67,38 @@ export const getTokensFromNetwork = (network: CustomChainInfo): TokenItemType[] 
 
 // other chains, oraichain
 const otherChainTokens = flatten(
-  chainInfos.filter(chainInfo => chainInfo.chainId !== 'Oraichain').map(getTokensFromNetwork)
+  chainInfos.filter((chainInfo) => chainInfo.chainId !== 'Oraichain').map(getTokensFromNetwork)
 );
 export const oraichainTokens: TokenItemType[] = getTokensFromNetwork(oraichainNetwork);
 
 export const tokens = [otherChainTokens, oraichainTokens];
 export const flattenTokens = flatten(tokens);
-export const tokenMap = Object.fromEntries(flattenTokens.map(c => [c.denom, c]));
-export const assetInfoMap = Object.fromEntries(flattenTokens.map(c => [c.contractAddress || c.denom, c]));
+export const tokenMap = Object.fromEntries(flattenTokens.map((c) => [c.denom, c]));
+export const assetInfoMap = Object.fromEntries(flattenTokens.map((c) => [c.contractAddress || c.denom, c]));
 export const cosmosTokens = uniqBy(
   flattenTokens.filter(
-    token =>
+    (token) =>
       // !token.contractAddress &&
       token.denom && token.cosmosBased && token.coinGeckoId
   ),
-  c => c.denom
+  (c) => c.denom
 );
 
 export const cw20Tokens = uniqBy(
   cosmosTokens.filter(
     // filter cosmos based tokens to collect tokens that have contract addresses
-    token =>
+    (token) =>
       // !token.contractAddress &&
       token.contractAddress
   ),
-  c => c.denom
+  (c) => c.denom
 );
 
 export const evmTokens = uniqBy(
   flattenTokens.filter(
-    token =>
+    (token) =>
       // !token.contractAddress &&
       token.denom && !token.cosmosBased && token.coinGeckoId && token.chainId !== 'kawaii_6886-1'
   ),
-  c => c.denom
+  (c) => c.denom
 );
